edbb3701797fa152046e38356cad2eee40c75a58
mnsh5 <hugo.orlando2050@gmail.com>
2024-04-20 17:06:41 -0300

    add Button component and update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+
 import { Message } from "./Message";
+import { Button } from "./Button";
 
 function App() {
   const [advice, setAdvice] = useState("");
 
   async function getAdvice() {
     try {
-      const res = await axios.get("https://api.adviceslip.com/advice");
-      setAdvice(res.data.slip);
+      const response = await axios.get("https://api.adviceslip.com/advice");
+      setAdvice(response.data.slip);
     } catch (error) {
       console.error("Error fetching advice:", error);
     }
@@ -20,9 +22,13 @@ function App() {
 
   return (
     <div className="bg-dark-blue-500 min-h-screen flex items-center justify-center">
-      <Message id={advice.id} advice={advice.advice} />
+      <div className="flex flex-col items-center justify-center">
+        <Message id={advice.id} advice={advice.advice} />
+        <Button getAdvice={getAdvice} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
