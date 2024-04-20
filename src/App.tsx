b72fe70e446ec9fd16a02bbec9213d1a8648d7fc
mnsh5 <hugo.orlando2050@gmail.com>
2024-04-20 17:00:31 -0300

    config tailwind and request from axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [advice, setAdvice] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  async function getAdvice() {
+    try {
+      const res = await axios.get("https://api.adviceslip.com/advice");
+      setAdvice(res.data.slip);
+    } catch (error) {
+      console.error("Error fetching advice:", error);
+    }
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return <></>;
 }
 
-export default App
+export default App;
